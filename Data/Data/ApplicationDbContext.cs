17a0c5660eb9fd3410b85dcf8db7d919e0c1eb30
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-07-14 14:38:13 +0300

    Add project files.

---
+++ Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace CollectionCards.Data.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
